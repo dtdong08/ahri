22ee543ecdc5c4bc07fc99e7903f587fbb90dcdd
dtdong08 <dangtrongdong08@gmail.com>
2024-10-06 15:18:04 +0700

    initial commit for minified branch
    turn all file in js directory to script.js, rename style/main.css to style.css

---
+++ README.md
@@ -0,0 +1,46 @@
+# Chilling Space
+
+Chilling Space is a simple static website featuring a continuous video of Ahri's HOL splash art as the background
+
+## Features
+- **Fullscreen video background** with seamless looping
+- **Customizable background music**
+- **Auto-hiding cursor** after 3 seconds of inactivity (customizable)
+- **Responsive design** for different screen sizes
+
+## Technologies
+- HTML
+- CSS
+- JavaScript
+- [toastr](https://github.com/CodeSeven/toastr)
+
+## Demo
+Checkout the live demo [here](https://dtdong08.github.io/chilling-space)
+
+## Usage
+- Press the **F** key or **double-click** to toggle fullscreen mode
+- The cursor hides automatically after 3 seconds of inactivity
+- Press the **D** key to adjust the delay time to hide the cursor
+- Press the **M** key to upload a file for background music
+- Press the **Spacebar** key or **click once** to pause/play the background music
+- Press the **>** key or **<** key to increase or decrease the volume of the background music
+  
+> Sorry, but I now have no idea how to implement some features on no-keyboard devices
+
+## To-dos
+- ~~Handle the responsive design better~~
+- ~~Support fullscreen experience on mobile browsers~~
+- ~~Add background music~~
+- ~~Add an option for users to adjust the cursor hide delay~~
+- ~~Enable users to upload a file for background music~~
+
+- Enable users to set a YouTube video as the background
+- Enable users to set background music using a YouTube link
+- Integrate a music visualizer
+> These three features will be available in dynamic version of this project
+
+## Credit
+- The video is sourced from [here](https://www.youtube.com/watch?v=MRJH95ltQAU) and downloaded using [y2mate](https://www.y2mate.com)
+
+## License
+This project is under the GPL-3.0 license.
---
+++ script.js
@@ -0,0 +1,148 @@
+// Get DOM elements
+const audioSource     = document.getElementById('audioSource');
+const musicUpload     = document.getElementById('musicUpload');
+const backgroundMusic = document.getElementById('backgroundMusic');
+
+// For cursor hide function
+let IDLE_TIME = localStorage.getItem('IDLE_TIME') ? parseFloat(localStorage.getItem('IDLE_TIME')) : 3000;
+let timeout = setTimeout(hideCursor, IDLE_TIME);
+
+// For volume adjust function
+const VOLUME_STEP = 0.01; // 1%
+
+// For fullscreen function
+let lastTouchTime = 0;
+
+// toastr configuration
+toastr.options = {
+	closeButton: false,
+	debug: false,
+	newestOnTop: true,
+	progressBar: true,
+	positionClass: "toast-top-right",
+	preventDuplicates: false,
+	onclick: null,
+	showDuration: "300",
+	hideDuration: "1000",
+	timeOut: "5000",
+	extendedTimeOut: "1000",
+	showEasing: "swing",
+	hideEasing: "linear",
+	showMethod: "fadeIn",
+	hideMethod: "fadeOut"
+}
+
+function togglePlayPause() {
+	if (backgroundMusic.paused) {
+		backgroundMusic.play();
+	} else {
+		backgroundMusic.pause();
+	}
+}
+
+function hideCursor() {
+	document.body.classList.add('hide-cursor');
+}
+
+function showCursor() {
+	document.body.classList.remove('hide-cursor');
+	clearTimeout(timeout);
+	timeout = setTimeout(hideCursor, IDLE_TIME);
+}
+
+function setIdleTime() {
+	const userInput = prompt('Enter the idle time in seconds:', IDLE_TIME / 1000);
+	const newIdleTime = parseFloat(userInput);
+
+	if (Number.isNaN(newIdleTime)) {
+		toastr.error('Please enter a valid number');
+	} else if (newIdleTime < 1) {
+		toastr.error('Idle time must be at least 1 second');
+	} else {
+		IDLE_TIME = newIdleTime * 1000;
+		localStorage.setItem('IDLE_TIME', IDLE_TIME);
+		if (IDLE_TIME == 1) {
+			toastr.success('Idle time set to 1 second');
+		} else {
+			toastr.success(`Idle time set to ${IDLE_TIME / 1000} seconds`);
+		}
+	}
+}
+
+function toggleFullscreen() {
+	if (!document.fullscreenElement) {
+		if (document.documentElement.requestFullscreen) {
+			document.documentElement.requestFullscreen();
+		} else if (document.documentElement.mozRequestFullScreen) {    // Firefox
+			document.documentElement.mozRequestFullScreen();
+		} else if (document.documentElement.webkitRequestFullscreen) { // Chrome, Safari, and Opera
+			document.documentElement.webkitRequestFullscreen();
+		} else if (document.documentElement.msRequestFullscreen) {     // IE/Edge
+			document.documentElement.msRequestFullscreen();
+		}
+	} else {
+		if (document.exitFullscreen) {
+			document.exitFullscreen();
+		} else if (document.mozCancelFullScreen) {  // Firefox
+			document.mozCancelFullScreen();
+		} else if (document.webkitExitFullscreen) { // Chrome, Safari, and Opera
+			document.webkitExitFullscreen();
+		} else if (document.msExitFullscreen) {     // IE/Edge
+			document.msExitFullscreen();
+		}
+	}
+}
+
+function adjustVolume(change) {
+	let newVolume = backgroundMusic.volume + change;
+	newVolume = Math.max(0.0, Math.min(1.0, newVolume));
+	backgroundMusic.volume = newVolume;
+	toastr.info(`Volume: ${(newVolume * 100).toFixed(0)}%`);
+}
+
+function handleMusicUpload(event) {
+	const file = event.target.files[0];
+	
+	if (file) {
+		const fileURL = URL.createObjectURL(file);
+		audioSource.src = fileURL;
+		backgroundMusic.load();
+		backgroundMusic.play();
+		toastr.success(`Now playing ${file.name}`);
+	}
+}
+
+document.addEventListener('click', togglePlayPause);
+
+document.addEventListener('mousemove', showCursor);
+
+document.addEventListener('dblclick', (e) => {
+	e.preventDefault();
+	toggleFullscreen();
+});
+
+document.addEventListener('touchstart', (e) => {
+	const currentTime = new Date().getTime();
+	const timeSinceLastTouch = currentTime - lastTouchTime;
+
+	if (timeSinceLastTouch < 300 && timeSinceLastTouch > 0) {
+		e.preventDefault();
+		toggleFullscreen();
+	}
+
+	lastTouchTime = currentTime;
+}, { passive: false });
+
+musicUpload.addEventListener('change', handleMusicUpload);
+
+document.addEventListener('keydown', (e) => {
+	if (e.code === 'Space') {
+		e.preventDefault();
+		togglePlayPause();
+	} 
+	else if (e.key === 'f' || e.key === 'F') toggleFullscreen();
+	else if (e.key === 'm' || e.key === 'M') musicUpload.click();
+  else if (e.key === 'd' || e.key === 'D') setIdleTime();
+	else if (e.key === '<') adjustVolume(-VOLUME_STEP);
+  else if (e.key === '>') adjustVolume(VOLUME_STEP);
+});
